6c1e4caa82ac926af0a7b546c5ac763e33e30c60
Сидоренко Славик <termenator2001@gmail.com>
2019-08-21 14:08:37 +0300

    111

--- ControlWork/Program.cs
+++ ControlWork/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace ControlWork
 {
+    struct city
+    {
+        public int population;
+        public string name;
+        public int den;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter");
+            //string text = Console.ReadLine();
+            string text = "Kharkiv = 234235,345;Kiev = 3465688,456;Vegas = 7809078,908";
+            string[] result = text.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            city[] cities = new city[result.Length];
+            for (int i = 0; i < result.Length; i++)
+            {
+                string[] nameSplit = result[i].Split('=');
+                string[] valueSplit = nameSplit[i].Split(',');
+                string b = nameSplit[0];
+                int a = int.Parse(valueSplit[0]);
+                int c = int.Parse(valueSplit[1]);
+            }
+            for (int i = 0; i < Length; i++)
+            {
+
+            }
         }
     }
 }
